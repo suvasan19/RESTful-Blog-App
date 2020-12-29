7abf68b3e057e7b08f12e7567699793e70b145b4
suvasan19 <suvasan19@gmail.com>
2020-12-29 17:06:55 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,117 @@
+var expressSanitizer = require('express-sanitizer'),
+	methodOverride = require('method-override'),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	express = require('express'),
+	app = express();
+
+//App Config
+mongoose.connect('mongodb://localhost/restful_blog_app', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false
+});
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSanitizer());
+app.use(methodOverride('_method'));
+
+//Mongoose/Model Config
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: {
+		type: String,
+		default:
+			'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/ways-to-make-coffee-super-healthy-1296x728-feature.jpg?w=1155&h=1528'
+	},
+	body: String,
+	created: { type: Date, default: Date.now }
+});
+var Blog = mongoose.model('Blog', blogSchema);
+
+//RESTful Routes
+
+app.get('/', function(req, res) {
+	res.redirect('/blogs');
+});
+
+//Index Route
+app.get('/blogs', function(req, res) {
+	Blog.find({}, function(err, blogs) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('index', { blogs: blogs });
+		}
+	});
+});
+
+//New Route
+app.get('/blogs/new', function(req, res) {
+	res.render('new');
+});
+
+//Create Route
+app.post('/blogs', function(req, res) {
+	req.body.blog.body = req.sanitize(req.body.blog.body);
+
+	Blog.create(req.body.blog, function(err, blog) {
+		if (err) {
+			res.render('new');
+		} else {
+			res.redirect('/blogs');
+		}
+	});
+});
+
+//Show route
+app.get('/blogs/:id', function(req, res) {
+	Blog.findById(req.params.id, function(err, foundBlog) {
+		if (err) {
+			res.redirect('/blogs');
+		} else {
+			res.render('show', { blog: foundBlog });
+		}
+	});
+});
+
+//Edit Route
+app.get('/blogs/:id/edit', function(req, res) {
+	req.body.blog.body = req.sanitize(req.body.blog.body);
+
+	Blog.findById(req.params.id, function(err, foundBlog) {
+		if (err) {
+			res.redirect('/blogs');
+		} else {
+			res.render('edit', { blog: foundBlog });
+		}
+	});
+});
+
+//Update Route
+app.put('/blogs/:id', function(req, res) {
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updated) {
+		if (err) {
+			res.redirect('/blogs');
+		} else {
+			res.redirect('/blogs/' + req.params.id);
+		}
+	});
+});
+
+//Delete Route
+app.delete('/blogs/:id', function(req, res) {
+	Blog.findByIdAndRemove(req.params.id, function(err, removed) {
+		if (err) {
+			res.redirect('/blogs' + req.params.id);
+		} else {
+			res.redirect('/blogs');
+		}
+	});
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+	console.log('Blog server Has Started!');
+});
